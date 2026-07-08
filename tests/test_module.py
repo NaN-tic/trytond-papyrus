e9d90bbcf523ca33add93a8aaa503ea5c24abc0c
Albert Cervera i Areny <albert@nan-tic.com>
2026-07-09 00:00:35 +0200

    Add 'content' field to the new tree view only if attachment_content module is activated.

--- tests/test_module.py
+++ tests/test_module.py
@@ -25,7 +25,9 @@ class PapyrusCompanyTestMixin(CompanyTestMixin):
 class PapyrusTestCase(PapyrusCompanyTestMixin, ModuleTestCase):
     'Test Papyrus module'
     module = 'papyrus'
-    extras = ['account_invoice', 'html_report', 'purchase', 'stock']
+    extras = [
+        'account_invoice', 'attachment_content', 'html_report', 'purchase',
+        'stock']
 
     @with_transaction()
     def test_reports_execute(self):
